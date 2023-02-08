79bbf3330852f87e91a8de7f26ddd6c4ab3b810a
gonglongfei <gonglongfei@qianxin.com>
2023-02-08 15:05:19 +0800

    fix

--- a.py
+++ a.py
@@ -1,5 +1,32 @@
-def test():
-    pass
-a = 1
-b = 2
-c = [a,b]
+import schema
+
+
+class EventSchema(schema.Schema):
+
+    def validate(self, data, _is_event_schema=True):
+        data = super(EventSchema, self).validate(data, _is_event_schema=False)
+        if _is_event_schema and data.get("minimum", None) is None:
+            data["minimum"] = data["capacity"]
+        return data
+
+
+events_schema = schema.Schema({
+    str: EventSchema({
+        "capacity": int,
+        schema.Optional("minimum"): int,  # default to capacity
+    })
+})
+
+text = {
+    "event1": {
+        "capacity": 1
+    },
+    "event2": {
+        "capacity": 2,
+        "minimum": 3
+    }
+}
+events = events_schema.validate(text)
+
+assert events["event1"]["minimum"] == 1  # == capacity
+assert events["event2"]["minimum"] == 3
